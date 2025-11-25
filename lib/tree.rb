8b7e7c13f195abcdfbb532790fcc339550bf95b4
its-namami <164639260+its-namami@users.noreply.github.com>
2025-11-25 17:26:23 +0100

    feat: add #delete

--- lib/tree.rb
+++ lib/tree.rb
@@ -14,7 +14,7 @@ class Tree
 
   # Tree will become unbalanced over time with this
   def insert(data, node = root)
-    return if data.eql?(node.data)
+    return if data == node.data
 
     case data <=> node.data
     when -1, 0
@@ -30,7 +30,61 @@ class Tree
 
   alias << insert
 
-  def pretty_print(node = @root, prefix = '', is_left = true)
+  def delete(value)
+    parent, half = find_parent(value)
+
+    unless parent
+      return unless value == root.data
+
+      root_val = root.data
+
+      if root.left && root.right
+        next_highest_parent = smallest_node_parent(root.right)
+        data = next_highest_parent&.left&.data
+
+        if data
+          root.data = next_highest_parent.left.data
+          next_highest_parent.left = nil
+        else
+          root.right.left = root.left.dup
+          self.root = root.right
+        end
+      elsif root.left || root.right
+        self.root = root.left || root.right
+      else
+        self.root = nil
+      end
+
+      return root_val
+
+    end
+
+    node = parent.public_send(half)
+    node_val = node.data
+
+    if node.left && node.right
+      next_highest_parent = smallest_node_parent(node.right)
+      data = next_highest_parent&.left&.data
+
+      if data
+        parent.public_send(half).data = next_highest_parent.left.data
+        next_highest_parent.left = nil
+      else
+        parent.public_send(half).right.left = node.left.dup
+        parent.public_send("#{half}=", node.right)
+      end
+    elsif node.left || node.right
+      parent.public_send("#{half}=", node.left || node.right)
+    else
+      parent.public_send("#{half}=", nil)
+    end
+
+    node_val
+  end
+
+  def pretty_print(node = root, prefix = '', is_left = true)
+    return '<empty>' if root.nil?
+
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
@@ -80,5 +134,27 @@ class Tree
     right = build_tree(sorted_array, array_segment.drop(half + 1))
 
     Node.new(root, left, right)
+  end
+
+  def smallest_node_parent(node = root)
+    if node.left&.left.nil?
+      return node
+    elsif node && node.left.nil?
+      return nil
+    end
+
+    smallest_node_parent(node.left)
+  end
+
+  def find_parent(value, node = root)
+    return if node.nil?
+    return [node, :left] if node&.left&.data == value
+    return [node, :right] if node&.right&.data == value
+
+    if value < node.data
+      find_parent(value, node.left)
+    elsif value > node.data
+      find_parent(value, node.right)
+    end
   end
 end
